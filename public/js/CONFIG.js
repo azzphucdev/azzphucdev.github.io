60346569f3e583f2d351ba780fcd30babe087bc8
azzphucdev <97656819+azzphucdev@users.noreply.github.com>
2022-01-13 14:24:58 +0700

    Update CONFIG.js

--- public/js/CONFIG.js
+++ public/js/CONFIG.js
@@ -1,54 +1,55 @@
 const __DATA__ = {
     'config': {
-        'title': 'INFO|Nguyễn Hữu Đạt',
+        'title': 'INFO|Ngô Sỹ Phúc',
         'favicon': 'home.png',
     },
     'cover-image': 'bia.jpg',
     'avatar-image': 'avatar.jpg',
-    'name': 'Nguyễn Hữu Đạt',
+    'name': 'Ngô Sỹ Phúc',
     'skills': [
         'Developer',
+        'Sniffer',
         'Designer',
     ],
     'bio-storys': [{
         'image': 'admin.png',
         'content': 'Giới thiệu',
-        'author': 'Chào Bạn 👋,tui sống tại TP Hà Nội',
+        'author': 'Chào Bạn 👋,tui sống tại Nghệ An ( Vietnamese )',
     }, ],
     'links': [{
             'image': 'facebook.jpg',
             'title': 'Facebook',
-            'show-value': '@mrdatdepzaii',
-            'value': 'https://www.facebook.com/MrDatDepZaii/',
+            'show-value': '@azzphuc',
+            'value': 'https://www.facebook.com/azz.phuc',
             'type': 0,
         },
         {
             'image': 'zalo.jpg',
             'title': 'Zalo',
-            'show-value': 'Nguyễn Hữu Đạt',
-            'value': 'https://zalo.me/mrdatdepzai',
+            'show-value': 'Ngô Sỹ Phúc',
+            'value': 'https://zalo.me/0363105920',
             'type': 0,
         },
 
         {
             'image': 'gmail.jpg',
             'title': 'Gmail',
-            'show-value': 'dat037sk@gmail.com',
-            'value': 'dat037sk@gmail.com',
+            'show-value': 'siphuc034@gmail.com',
+            'value': 'siphuc034@gmail.com',
             'type': 1,
         },
         {
             'image': 'phone.jpg',
             'title': 'MB Bank',
-            'show-value': '0866051437',
-            'value': '0866051437',
+            'show-value': '99992059999',
+            'value': '99992059999',
             'type': 1,
         },
         {
             'image': 'momo.jpg',
             'title': 'MoMo',
-            'show-value': '0866051437',
-            'value': '0866051437',
+            'show-value': '0355915217',
+            'value': '0355915217',
             'type': 1,
         }
     ]
