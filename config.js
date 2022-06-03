318628bae48096b38d92c5286b8e1ef1e83254f8
azzphucdev <97656819+azzphucdev@users.noreply.github.com>
2022-06-03 09:16:12 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'He luu cậu!',
+    introTitle: 'Chào Cậu !',
     introDesc: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
     btnIntro: 'Hi Hi',
     title: 'Cậu yêu tớ có phải không nào ._.',
@@ -13,78 +13,5 @@ const CONFIG = {
     messDesc: `Liên hệ với tớ nhé.
 Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàoooooo`,
     btnAccept: 'Okiii lunn <3',
-    messLink: 'https://trantienvn.github.io/tien'
-};
-const __DATA__ = {
-    'cover-image': 'biaz.jpg',
-    'avatar-image': 'tien.png',
-    'name': 'Trần Văn Tiến',
-    'skills': [
-        'Stolen',
-        'Stolen',
-        'Chôm',
-        'Chôm'
-    ],
-    'bio-storys': [{
-        'image': 'avatar.png',
-        'content': 'Giới thiệu',
-        'author': 'Chào Bạn 👋, tui là Tiến sống tại TP Phổ Yên',
-    }, ],
-    'links': [{
-            'image': 'facebook.jpg',
-            'title': 'Facebook',
-            'show-value': '@trantien',
-            'value': 'https://www.facebook.com/trantienvpn/',
-            'type': 0,
-        },
-        {
-            'image': 'mess.jpg',
-            'title': 'Messenger',
-            'show-value': 'Trần Tiến',
-            'value': 'https://m.me/trantienvpn',
-            'type': 0,
-        },
-        {
-            'image': 'zalo.jpg',
-            'title': 'Zalo',
-            'show-value': 'Trần Tiến',
-            'value': 'https://zalo.me/0382652361',
-            'type': 0,
-        },
-        {
-            'image': 'tele.jpg',
-            'title': 'Telegram',
-            'show-value': 'Trần Tiến',
-            'value': 'https://t.me/tienvpn',
-            'type': 0,
-        },
-        {
-            'image': 'gmail.jpg',
-            'title': 'Gmail',
-            'show-value': 'tientn2005@gmail.com',
-            'value': 'tientn2005@gmail.com',
-            'type': 1,
-        },
-        {
-            'image': 'vtm.jpg',
-            'title': 'Viettel Money',
-            'show-value': '0382652361',
-            'value': 'https://vtpay.page.link/Msbw',
-            'type': 0,
-        },
-        {
-            'image': 'momo.jpg',
-            'title': 'MoMo',
-            'show-value': '0382652361',
-            'value': 'https://me.momo.vn/trantienvpn',
-            'type': 0,
-        },
-        {
-            'image': 'mb.jpg',
-            'title': 'MB Bank',
-            'show-value': '0382652361',
-            'value': '0382652361',
-            'type': 1,
-        }
-    ]
+    messLink: 'https://azzphuc.online/info'
 };
